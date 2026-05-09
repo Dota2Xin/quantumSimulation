710fb0e5d5df7d15d1d360076d3068ceb6d8fba2
Dota2Xin <gabrielkumar@hotmail.com>
2026-05-09 17:25:29 -0500

    we'll have to do energies, after I write up ewald summation I'll do it... maybe

---
+++ DFT/kSpaceLDALocalPseudo/calculateEnergies.py
@@ -0,0 +1,7 @@
+from blockDavidson import *
+import math
+import numpy as np
+from scipy.signal import fftconvolve
+
+def calculateEnergy(density,wavefunctions, atomicPositions, atomicNumbers):
+    return 0
