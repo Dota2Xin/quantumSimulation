5c97461537870680cff58254e4a1d771d2cb69ce
Dota2Xin <gabrielkumar@hotmail.com>
2026-05-10 11:15:11 -0500

    skeleton for energies

--- DFT/kSpaceLDALocalPseudo/calculateEnergies.py
+++ DFT/kSpaceLDALocalPseudo/calculateEnergies.py
@@ -5,3 +5,21 @@ from scipy.signal import fftconvolve
 
 def calculateEnergy(density,wavefunctions, atomicPositions, atomicNumbers):
     return 0
+
+def calcEwald():
+    return 0
+
+def ewaldTranslation():
+    return 0
+
+def ewaldG():
+    return 0
+
+def hartreeEnergy():
+    return 0
+
+def kineticEnergy():
+    return 0
+
+def exchangeCorrelationEnergy():
+    return 0
