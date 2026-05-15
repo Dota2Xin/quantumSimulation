Now instead of too big its too low, the order we returned things was bad and that's fixed, now presumably there is bad normalization happening. ???

DFT/kSpaceLDALocalPseudo/blockDavidson.py
@@ -45,8 +45,10 @@ def blockDavidson(l,m,stateSize,qGridSmall, actHamiltonian, hamiltonianArgs):
         ratio=np.abs(np.linalg.norm(res[:, :l])-temp)/np.linalg.norm(res[:, :l])
         temp=np.linalg.norm(res[:, :l])
 
-
-    return eigval[:l], ritz[:, :l]
+    gridShape=qGridSmall.shape[:-1]
+    eigenfuncs=ritz[:,:l]
+    eigenfuncs = np.array([eigenfuncs[:, i].reshape(gridShape) for i in range(l)])
+    return eigval[:l], eigenfuncs
 
 def getDiagonal(qGridSmall, hamiltonianArgs):
     k=hamiltonianArgs['k']

DFT/kSpaceLDALocalPseudo/calculateEnergies.py
@@ -118,7 +118,7 @@ def ewaldG(qGridBig, atomicPositions,atomicNumbers, eta, cellVol):
     return 2*np.pi*np.sum(main)/cellVol
 
 def hartreeEnergy(qGridBig, density):
-    main=np.abs(density)*np.abs(density)
+    main=np.real(np.abs(density)*np.abs(density))
     qNorm=np.linalg.norm(qGridBig, axis=-1)
     main=np.divide(main, (qNorm**2.0), out=np.zeros_like(density), where=(qNorm!=0))
 
@@ -127,7 +127,7 @@ def hartreeEnergy(qGridBig, density):
 def kineticEnergy(wavefuncs, qGridSmall, k):
     T=0
     for i in range(len(wavefuncs)):
-        state=wavefuncs[i]
+        state=wavefuncs[:,i]
         T+=np.sum(np.abs(state)*0.5 * (np.linalg.norm((qGridSmall + k), axis=-1) ** 2.0) * np.abs(state))
     return T
 

DFT/kSpaceLDALocalPseudo/handleSCF.py
@@ -153,11 +153,11 @@ def mainSCFLoop(initialConditions):
     energyInputDict['kGrid']=[[0]]
     prevEnergy=0
     while relDiff > tol:
-        wavefuncs, energies= solveSchrodinger(solveSchrodingerInputDict)
+        energies, wavefuncs= solveSchrodinger(solveSchrodingerInputDict)
         oldDensity=np.copy(density)
         density=np.zeros_like(oldDensity)
         for i in range(occupations):
-            density+=2*calcDensity(wavefuncs[i], n1,n2,n3, cellVol)
+            density+=2*calcDensity(wavefuncs[:,i], n1,n2,n3, cellVol)
         print("CURR SOMETHING OR OTHER:")
         print(f"Mean Density Data: {getMeanDensity(density, n1, n2, n3)}")
         print(f"Mean Density True:{np.sum(atomicNumbers)/cellVol}")