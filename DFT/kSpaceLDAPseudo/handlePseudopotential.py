775b81c6148e818ae22af0cf24d9c3281997fbc7
Dota2Xin <gabrielkumar@hotmail.com>
2026-05-19 19:46:13 -0500

    pseudo skeleton, been reading the Vanderbilt and Hamann papers, slowly getting how the pseudopotentials work.

---
+++ DFT/kSpaceLDAPseudo/handlePseudopotential.py
@@ -0,0 +1,14 @@
+def getProjectors():
+    return 0
+
+def actProjectors():
+    return 0
+
+def getLocalPart():
+    return 0
+
+def getCoreDensity():
+    return 0
+
+def handleCoreDensity():
+    return 0
